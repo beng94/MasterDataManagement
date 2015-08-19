344af62fbe08503069eedec2bcd10ff6d16f9c46
Gedai Tamás Bence <gedai.bence@gmail.com>
2015-08-19 13:33:21 +0200

    test: python program fixed, proper indexing implemented

--- test/test.py
+++ test/test.py
@@ -1,15 +1,19 @@
-lines = []
+#!/usr/bin/python
+
+lines_dict = {}
 
 with open("../training_data.csv") as file:
     for line in file:
-        lines.append(line)
+        chunks = line.replace('\n', '').replace('\r', '').split(',')
+        lines_dict[int(chunks[0])] = ",".join(chunks[1:])
 
 file_w = open("duplicates.txt", 'w+')
 with open("../training_ground_truth.csv") as file:
     for line in file:
         chunks = line.split(",")
-        file_w.write(lines[int(chunks[0])])
-        file_w.write(lines[int(chunks[1])])
+        file_w.write(chunks[0] + "," + lines_dict[int(chunks[0])] + '\n')
+        file_w.write(chunks[1].replace('\n', '').replace('\r','') + "," + lines_dict[int(chunks[1])] + '\n')
+    print "Program terminated"
 file_w.close()
 
 
