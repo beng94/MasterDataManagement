d1f38033115643627615ddf491aa9724aeac51b7
Gedai Tamás Bence <gedai.bence@gmail.com>
2015-08-20 10:33:38 +0200

    statistics.cpp: read_training_data implemented
    Note: CAddres ctor is not called, because there's a segfault in it,
    therefore neither the Entity's ctor is called.

--- main/statistics.cpp
+++ main/statistics.cpp
@@ -1,7 +1,48 @@
+#include <stdexcept>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
+
 #include "statistics.hpp"
+#include "../csvparser.h"
+
+static char** parse_line(CsvParser *parser)
+{
+    CsvRow *row = CsvParser_getRow(parser);
+    if(row == NULL) throw std::out_of_range("");
+
+    return CsvParser_getFields(row);
+}
+
+void Statistics::read_training_data(std::vector<Entity>& vec)
+{
+    CsvParser *parser = CsvParser_new(training_data_filename.c_str(), ",", 0);
+    std::vector<Entity> entities_vec;
+
+    while(true)
+    {
+        try
+        {
+            char **rowFields = parse_line(parser);
+
+            int id = boost::lexical_cast<int>(rowFields[0]);
+            Name name(trim(rowFields[1]));
+            //CAddress address(trim(rowFields[2]));
+            //Taxonomies taxonomies(trim(rowFields[3]));
+            //Entity entity(id, name, address, taxonomies);
+
+            //vec.push_back(entity);
+        }
+        catch (std::out_of_range& oor) { break; }
+    }
+    CsvParser_destroy(parser);
+}
 
 bool Statistics::calculate_oddsVector()
   {
     //TODO: LOTS OF FUMCTIONS ABOVE
+    std::vector<Entity> entities;
+    entities.reserve(450000);
+    read_training_data(entities);
+
     return true;
   }
